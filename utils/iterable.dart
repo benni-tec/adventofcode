8c4ed11410cec9865052a83f614b35cb9574fd99
benni-tec <me@benni-tec.de>
2023-12-03 00:11:52 +0100

    added permutation sorting for iterables

--- utils/iterable.dart
+++ utils/iterable.dart
@@ -10,3 +10,12 @@ extension Empty on Iterable<String> {
   Iterable<String> nonEmpties() => where((element) => element.isNotEmpty);
   Iterable<String> empties() => where((element) => element.isEmpty);
 }
+
+extension PermutationSorting<T> on Iterable<T> {
+  Iterable<T> sorted(int Function(T a, T b) compare) {
+    final permutation = List.generate(length, (index) => index);
+    permutation.sort((ai, bi) => compare(elementAt(ai), elementAt(bi)));
+
+    return permutation.map((e) => elementAt(e));
+  }
+}
